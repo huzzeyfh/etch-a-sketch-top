c62b7996c49c167f1bcee4ba999196e163907dfd
huzzeyfh <hudzaifah7103@gmail.com>
2025-09-13 20:59:12 +0700

    Create the real grid, fix a long line grid
    Recall basic foundations

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 //HTML variables
 const container = document.querySelector('.container');
 
+//container styles
+container.classList.add('container');
+
 //Add the grids
-for (i = 0; i < 16; i++) {
+for (i = 0; i < 256; i++) {
     const addDivs = document.createElement('div');
-    addDivs.className = "grids";
-    addDivs.style.padding = "16px";
+    addDivs.className = 'grids';
     container.appendChild(addDivs);
 }
 
+//Hover
+const grid = document.querySelectorAll('.grids');
+grid.style.backgroundColor = 'blue';
+// console.log(grid);
+// grid.addEventListener('mouseover', () => {
+//     grid.style.backgroundColor = 'blue';
+// });
+
+// grid.addEventListener('mouseout', () => {
+//     grid.style.backgroundColor = '';
+// });
