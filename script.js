896c8425bb2e7f7f3b014a8776fefe4e77b7dd86
huzzeyfh <hudzaifah7103@gmail.com>
2025-09-14 13:34:03 +0700

    Make the button working, but the hover didn't
    Got 'return' misunderstanding

--- script.js
+++ script.js
@@ -2,17 +2,41 @@
 const button = document.querySelector('.button');
 const container = document.querySelector('.container');
 
-
-//variables
-let gridValue;
-let i = gridValue;
-
 //Add the grids
 for (i = 0; i < 256; i++) {
     const addDivs = document.createElement('div');
     addDivs.className = 'grids';
     container.appendChild(addDivs);
 }
+
+//Button
+button.addEventListener('click', function promptSize() {
+    //Remove grids
+    const removeGrids = document.querySelectorAll('.grids');
+    removeGrids.forEach((removeGrid) => {
+        container.removeChild(removeGrid);
+    });
+
+    //Desired grid-size
+    const gridValue = parseInt(prompt('Input your desired grid-size (Max : 100)', ''), 10);
+
+    if (gridValue >= 1 && gridValue < 100) {
+        //Add grids
+        for (i = 0; i < gridValue * gridValue; i++) {
+            const addDivs = document.createElement('div');
+            addDivs.className = 'grids';
+            addDivs.style.height = `${640 / gridValue}px`;
+            addDivs.style.width = `${640 / gridValue}px`;
+            container.appendChild(addDivs);
+        }
+    } else if (gridValue >= 100) {
+        alert('Maximum 100!');
+    } else {
+        alert('Please input number');
+    }
+    
+    console.log(grids);
+});
 
 //Hover
 const grids = document.querySelectorAll('.grids');
@@ -26,23 +50,4 @@ grids.forEach((grid) => {
     })
 });
 
-//Button
-button.addEventListener('click', function promptSize() {
-    //Remove grids
-    grids.forEach((grid) => {
-        container.removeChild(grid);
-    });
-
-    //Desired grid-size
-    const gridValue = prompt('Input your desired grid-size (Max : 100)', '');
-
-    if (gridValue >= 1 && gridValue < 100) {
-        return;
-    } else if (gridValue >= 100) {
-        alert('Maximum 100!');
-    } else {
-        alert('Please input number');
-    }
-
-    //Add grids
-});
+console.log(grids);
